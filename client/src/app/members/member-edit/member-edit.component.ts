fbfc46f28ffaf7f7dfbfa34adcddc229c0d52a1d
Juan Vargas <jvargas@lacolonial.com.do>
2023-08-10 16:36:24 -0400

    Thrusday Afternoon Commit.

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,23 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Member } from 'src/app/_models/member';
 import { User } from 'src/app/_models/User';
 import { AccountService } from 'src/app/_services/account.service';
 import { MembersService } from 'src/app/_services/members.service';
 import { take } from 'rxjs';
+// import { Gallery, GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 
 @Component({
   selector: 'app-member-edit',
   templateUrl: './member-edit.component.html',
   styleUrls: ['./member-edit.component.css']
 })
-export class MemberEditComponent {
+export class MemberEditComponent implements OnInit {
   member?: Member;
   user: User | null = null;
+  // images: GalleryItem[] = [];
 
   constructor(private accountService: AccountService, private membersService: MembersService) {
     this.accountService.currentUser$.pipe(take(1)).subscribe({
       next: user => this.user = user
     })
+  }
+
+  ngOnInit() {
+    this.loadMember();
+  }
+
+  loadMember() {
+    if (!this.user) return;
+    this.membersService.getMember(this.user.username).subscribe({
+      next: member => this.member = member
+    });
   }
 
 }
