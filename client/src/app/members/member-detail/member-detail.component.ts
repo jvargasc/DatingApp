59ab05fc756073945821905adc08c770d4da89d5
Juan Vargas <jvargasc@gmail.com>
2023-08-27 21:16:02 -0400

    Sunday Evening Commit.

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,29 +1,51 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Gallery, GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
-import { TabsModule } from 'ngx-bootstrap/tabs';
+import { TabDirective, TabsModule, TabsetComponent } from 'ngx-bootstrap/tabs';
 import { TimeagoModule } from 'ngx-timeago';
 import { ToastrService } from 'ngx-toastr';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
+import { MemberMessagesComponent } from '../member-messages/member-messages.component';
+import { MessageService } from 'src/app/_services/message.service';
+import { Message } from 'src/app/_models/message';
 
 @Component({
   selector: 'app-member-detail',
   standalone: true,
   templateUrl: './member-detail.component.html',
   styleUrls: ['./member-detail.component.css'],
-  imports: [ CommonModule, TabsModule, GalleryModule, TimeagoModule ]
+  imports: [ CommonModule, TabsModule, GalleryModule, TimeagoModule, MemberMessagesComponent ]
 })
 export class MemberDetailComponent implements OnInit{
+  @ViewChild('memberTabs') memberTabs?: TabsetComponent;
   member?: Member;
   images: GalleryItem[] = [];
+  activeTab?: TabDirective;
+  messages: Message[] = [];
 
-  constructor(private memberService: MembersService, private route: ActivatedRoute, private toastr: ToastrService) { }
+  constructor(private memberService: MembersService, private route: ActivatedRoute,
+              private toastr: ToastrService, private messageService: MessageService) { }
 
   ngOnInit() {
     this.loadMember();
   }
+
+  onTabActivated(data: TabDirective) {
+    this.activeTab = data;
+    if (this.activeTab.heading === 'Messages')
+      this.loadMessages();
+  }
+
+  loadMessages() {
+    if (this.member) {
+      this.messageService.getMessageThread(this.member.userName).subscribe({
+        next: messages => this.messages = messages
+      })
+    }
+  }
+
 
   loadMember() {
     const username = this.route.snapshot.paramMap.get('username');
