851f6cbb33d52afed0a08233333c6d3d006252d5
Juan Vargas <jvargas@lacolonial.com.do>
2023-09-01 16:51:10 -0400

    Friday Afternoon Commit.

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -24,7 +24,7 @@ public class AdminController : BaseApiController
             .Select(u => new
             {
                 u.Id,
-                u.UserName,
+                Username = u.UserName,
                 Roles = u.UserRoles.Select(r => r.Role.Name).ToList()
             })
             .ToListAsync();
